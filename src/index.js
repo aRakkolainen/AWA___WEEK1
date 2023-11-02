535509c0b4d40cc23158a1f76544c1b7598cd855
Aino <103110604+aRakkolainen@users.noreply.github.com>
2023-11-02 09:17:28 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -7,30 +7,27 @@ if (document.readyState !== "loading") {
     initializeCode();
   });
 }
-
-function initializeCode() {
+async function fetchDogImage(url) {
+  let response = await fetch(url);
+  let json = await response.json();
+  console.log(json);
+}
+async function initializeCode() {
   document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
-
+  let url = "https://dog.ceo/api/breed/";
   let app = document.getElementById("app");
   let container = document.createElement("div");
   container.setAttribute("class", "container");
-  /*<div class="wiki-item" >
-  <h1 class="wiki-header">Breed X</h1>
-  <div class="wiki-content">
-     <p class="wiki-text">
-       Some text about this breed.
-     </p>
-     <div class="img-container">
-       <img class="wiki-img" src="">
-     </div>
-  </div>
-</div> */
+  //Creating list of five possible breeds:
+  let breeds = ["Akita", "Chihuahua", "Labrador", "Samoyed", "Whippet"];
+  console.log(breeds);
   for (let i = 0; i < 5; i++) {
     let item = document.createElement("div");
     item.setAttribute("class", "wiki-item");
     let header = document.createElement("h1");
     header.setAttribute("class", "wiki-header");
-    header.innerText = "Breed " + (i + 1);
+    header.innerText = breeds[i];
+    //fetchDogImage(url);
     let content = document.createElement("div");
     content.setAttribute("class", "wiki-content");
     let text = document.createElement("p");
@@ -38,8 +35,15 @@ function initializeCode() {
     let imgContainer = document.createElement("div");
     imgContainer.setAttribute("class", "img-container");
     let img = document.createElement("img");
+    // Fetching the image:
+    let url =
+      "https://dog.ceo/api/breed/" + breeds[i].toLowerCase() + "/images/random";
+    let response = await fetch(url);
+    let dogJSON = await response.json();
+    let dog_img = dogJSON.message;
     img.setAttribute("class", "wiki-img");
-    img.setAttribute("src", "");
+    img.setAttribute("src", dog_img);
+
     imgContainer.appendChild(img);
     content.appendChild(text);
     content.appendChild(imgContainer);
