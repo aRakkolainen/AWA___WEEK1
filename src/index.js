96a43c564c93a91e8b74638bc899e2c77bb8f9f9
Aino <103110604+aRakkolainen@users.noreply.github.com>
2023-11-02 10:05:32 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function initializeCode() {
   let container = document.createElement("div");
   container.setAttribute("class", "container");
   //Creating list of five possible breeds:
-  let breeds = ["Akita", "Chihuahua", "Labrador", "Samoyed", "Whippet"];
+  let breeds = ["Akita", "Labrador", "Pitbull", "Samoyed", "Whippet"];
   console.log(breeds);
   for (let i = 0; i < 5; i++) {
     let item = document.createElement("div");
@@ -30,16 +30,25 @@ async function initializeCode() {
     content.setAttribute("class", "wiki-content");
     let text = document.createElement("p");
     text.setAttribute("class", "wiki-text");
-    text.innerText =
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque natus itaque voluptatem eos aspernatur necessitatibus facilis quidem assumenda cum, sequi perferendis aliquid laudantium nihil temporibus quos modi, nisi quis adipisci.";
+    let wikiurl =
+      "https://en.wikipedia.org/api/rest_v1/page/summary/" +
+      breeds[i].toLowerCase();
+    console.log(wikiurl);
+    let text_response = await fetch(wikiurl);
+    let summaryJSON = await text_response.json();
+    let summaryText = summaryJSON.extract;
+    text.innerText = summaryText;
+    //"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque natus itaque voluptatem eos aspernatur necessitatibus facilis quidem assumenda cum, sequi perferendis aliquid laudantium nihil temporibus quos modi, nisi quis adipisci.";
+    // fetching the sumary text from wikipedia
+
     let imgContainer = document.createElement("div");
     imgContainer.setAttribute("class", "img-container");
     let img = document.createElement("img");
     // Fetching the image:
     let url =
       "https://dog.ceo/api/breed/" + breeds[i].toLowerCase() + "/images/random";
-    let response = await fetch(url);
-    let dogJSON = await response.json();
+    let img_response = await fetch(url);
+    let dogJSON = await img_response.json();
     let dog_img = dogJSON.message;
     img.setAttribute("class", "wiki-img");
     img.setAttribute("src", dog_img);
